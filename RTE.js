38b16a5988bb968035a732332c9f643f88551561
D365Cde <100127843+D365Cde@users.noreply.github.com>
2022-02-21 10:11:28 +0000

    Add files via upload

---
+++ RTE.js
@@ -0,0 +1,9 @@
+{ 
+                "defaultSupportedProps": {
+                "font_names":"Brush Script MT/'Brush Script MT', cursive;Calibri/Calibri, Helvetica, sans-serif;Calibri Light/'Calibri Light', 'Helvetica Light', sans-serif;", 
+                "font_defaultLabel":"Brush Script MT", 
+                "fontSize_sizes":"8/8px;12/12px;20/20px;32/32px", 
+                "fontSize_defaultLabel":"20", 
+                "stickyStyle":{"font-size":"20px", "font-family":"'Brush Script MT', cursive"}  
+                }
+}
