acf5f29579b7dd3523d348fc1bbdacae640eced1
D365Cde <100127843+D365Cde@users.noreply.github.com>
2022-02-21 10:20:07 +0000

    Update RTE.js

--- RTE.js
+++ RTE.js
@@ -1,6 +1,6 @@
 { 
                 "defaultSupportedProps": {
-                "font_names":"Brush Script MT/'Brush Script MT', cursive;Calibri/Calibri, Helvetica, sans-serif;Calibri Light/'Calibri Light', 'Helvetica Light', sans-serif;", 
+                "font_names":"cursive;Calibri/Calibri, Helvetica, sans-serif;Calibri Light/'Calibri Light', 'Helvetica Light', sans-serif;", 
                 "font_defaultLabel":"Calibri", 
                 "fontSize_sizes":"8/8px;12/12px;20/20px;32/32px", 
                 "fontSize_defaultLabel":"20", 
